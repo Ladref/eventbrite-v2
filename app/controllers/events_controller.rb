57b1e38b0dd0a27f19563c3834c4bec8c83eca4b
ladref <louis.dreyfus@icloud.com>
2019-08-14 00:21:56 +0200

    let me die

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,17 +6,19 @@ class EventsController < ApplicationController
   end
 
   def new
+    @user = current_user
     @event = Event.create
   end
 
   def create
+    @user = current_user
     @event = Event.new(title: params[:title],
                         description: params[:description],
                         location: params[:location],
                         start_date: params[:start_date],
                         duration: params[:duration].to_i,
                         price: params[:price].to_i,
-                        user_id: current_user)
+                        user_id: @user.id)
     if @event.save
       flash[:success] = "The event has been saved."
       redirect_to @event
