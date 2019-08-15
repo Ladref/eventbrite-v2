8bdfd70bd360e4aafa174f0ad33f6b81e03f42c3
ladref <louis.dreyfus@icloud.com>
2019-08-15 21:49:46 +0200

    wip

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -12,28 +12,36 @@ class AttendancesController < ApplicationController
 
 
   def create
+    puts "You are in the create method"
+    puts "*"*40
     @event = Event.find(params[:event_id])
-    @attendance = Attendance.new(participant_id: current_user.id,
-                                 event_id: params[:event_id])
-    if @attendance.save
-      customer = Stripe::Customer.create({
+
+    customer = Stripe::Customer.create({
         email: params[:stripeEmail],
         source: params[:stripeToken],
       })
 
-      charge = Stripe::Charge.create({
+    charge = Stripe::Charge.create({
         customer: customer.id,
-        amount: Event.find(params[:event_id]).price,
+        amount: @event.price,
         description: 'Rails Stripe customer',
         currency: 'eur',
       })
 
+    @attendance = Attendance.new(participant_id: current_user.id,
+                                 event_id: params[:event_id],
+                               stripe_customer_id: charge.customer)
+
+    if @attendance.save
+      flash[:success] = "You are now registered to the event."
       redirect_to @event
     end
 
     rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_charge_path
-    end
+    flash[:error] = e.message
+    redirect_to new_charge_path
+
+
+  end
 
 end
---
+++ app/controllers/concerns/test.rb
@@ -0,0 +1,24 @@
+def create
+  @event = Event.find(params[:event_id])
+  @attendance = Attendance.new(participant_id: current_user.id,
+                               event_id: params[:event_id])
+  if @attendance.save
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: Event.find(params[:event_id]).price,
+      description: 'Rails Stripe customer',
+      currency: 'eur',
+    })
+
+    redirect_to @event
+  end
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
